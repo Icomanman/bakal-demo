3ced61256391c520172f51fdf80a4ace88d3f4b2
Icomanman <fred_barabs@yahoo.com>
2022-09-02 01:37:13 +0800

    feat: Search bar added - no functionality yet.

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,15 +1,15 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, Pressable } from 'react-native';
 import React from 'react';
 
 import { Feather } from '@expo/vector-icons';
 
 export default function Searchbar() {
   return (
-    <View className='flex-row my-1 py-2 bg-gray-100 rounded-lg'>
-      <Text className='font-bold text-gray-800 text-xl'>
-        Welcome home!
-      </Text>
-      <Feather name='search' size={24} color='rgb(17, 24, 39)' />
+    <View className='flex-row my-1 rounded items-center w-96 h-10'>
+      <TextInput color='rgb(209, 213, 219)' className='text-gray-800 bg-gray-100 text-lg px-2 w-10/12 h-full' placeholder='Search' keyboardType='default' />
+      <Pressable className='bg-gray-400 rounded justify-center items-center h-full w-2/12'>
+        <Feather name='search' size={24} color='rgb(17, 24, 39)' />
+      </Pressable>
     </View>
   )
 }
