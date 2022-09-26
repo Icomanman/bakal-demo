d11c36b60c1b1deccd76f651fffe1ec76ad657ea
Icomanman <fred_barabs@yahoo.com>
2022-09-26 23:11:17 +0800

    feat: adding comment section input.

--- components/Post.js
+++ components/Post.js
@@ -1,18 +1,50 @@
-import { Image, Pressable, View, Text } from 'react-native';
+import { Image, Pressable, View, Text, TextInput } from 'react-native';
 import { FontAwesome5, Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
 
 const icon_color = 'rgb(17, 24, 39)';
 
+const CommentInput = ({ comments }) => {
+  const [comment, setComment] = useState();
+  const sendComment = comment => {
+    comments.push({
+      user: 'User',
+      comment: comment
+    })
+    console.log(comment);
+    setComment(comment);
+  };
+  let tmp_input = '';
+  return (
+    <View className='flex-row items-center mx-1'>
+      <View className='w-80'>
+        <TextInput
+          numberOfLines={2}
+          placeholder='Add comment...'
+          maxLength={160}
+          multiline={true}
+          onChangeText={comment => tmp_input = comment} />
+      </View>
+      <View className='flex-row flex-grow justify-end'>
+        <Pressable className='justify-center mx-3' onPress={() => sendComment(tmp_input)}>
+          <Ionicons name="send" size={14} color={icon_color} />
+        </Pressable>
+      </View>
+    </View>
+  )
+};
+
 const Comments = ({ comments }) => {
-  const comment_header = comments.length > 1 ?
-    <Text className='mx-1 mt-1 text-gray-500'>See all {comments.length} comments</Text> :
-    <Text className='mx-1 mt-1 text-gray-500'>Add comment</Text>
+  const comment_header = comments.length > 0 ?
+    <Text className='mt-1 mb-1 text-gray-500'>{comments.length} {comments.length === 1 ? 'comment' : 'comments'}</Text> :
+    <CommentInput comments={comments} />
+  // <Text className='mx-1 mt-1 text-gray-500'>Add comment</Text>
 
   const user_comments = [];
   for (let i = 0; i < comments.length; i++) {
     user_comments.push(
       <View className='break-words' key={i}>
-        <Text className=' text-gray-800 m-1'>
+        <Text className=' text-gray-800'>
           <Text className='font-bold'>{comments[i].user}</Text>
           <Text> {comments[i].comment}</Text>
         </Text>
@@ -21,12 +53,12 @@ const Comments = ({ comments }) => {
   }
 
   return (
-    <View className='mx-1 mb-2'>
+    <View className='mx-2 mb-2'>
       {comment_header}
       {user_comments}
     </View>
   );
-}
+};
 
 const PostHeader = ({ post }) => (
   <View className='flex-row my-1 mx-2 items-center'>
@@ -50,11 +82,30 @@ const PostImage = ({ post }) => (
   </View>
 );
 
-const PostFooter = ({ post }) => (
-  <View>
+const PostFooter = ({ post }) => {
+  const [likes, setPostLikes] = useState(post.likes);
+  const [liked, setLiked] = useState(false);
+  const liked_icon = liked ? 'heart-sharp' : 'heart-outline';
+
+  const [comments, postComment] = useState(post.comments);
+
+  const updateLikes = is_liked => {
+    setLiked(is_liked);
+    setPostLikes(is_liked ? likes + 1 : post.likes);
+    // send post request:
+    setTimeout(() => {
+      console.log('POST');
+    }, 1000);
+  };
+
+  const updateComments = new_comment => {
+
+  };
+
+  return (<View>
     <View className='flex-row items-center'>
-      <Pressable className='my-2 mx-3 justify-center'>
-        <FontAwesome5 name="heart" size={24} color={icon_color} />
+      <Pressable className='my-2 mx-3 justify-center' onPress={() => updateLikes(!liked)}>
+        <Ionicons name={liked_icon} size={24} color={liked ? 'red' : icon_color} />
       </Pressable>
       <Pressable className='my-2 mx-3 justify-center'>
         <FontAwesome5 name="comment" size={24} color={icon_color} />
@@ -70,7 +121,7 @@ const PostFooter = ({ post }) => (
     </View>
 
     <View className='mx-2'>
-      <Text className='font-bold'>{post.likes} likes</Text>
+      <Text className='font-bold'>{likes} {likes > 1 || likes === 0 ? 'likes' : 'like'}</Text>
     </View>
 
     <View className='flex-row mx-2'>
@@ -78,10 +129,9 @@ const PostFooter = ({ post }) => (
       <Text className=''>{post.caption}</Text>
     </View>
 
-    <Comments comments={post.comments} />
-
-  </View>
-);
+    <Comments comments={comments} />
+  </View>)
+};
 
 export default function Post({ post }) {
   return (
