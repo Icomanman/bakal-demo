85ef37de90a438530389459aa9d0788152817620
Icomanman <fred_barabs@yahoo.com>
2022-09-26 13:00:27 +0800

    feat: header logo update.

--- components/Header.js
+++ components/Header.js
@@ -7,23 +7,18 @@ import { UserIcon, SearchIcon } from 'react-native-heroicons/solid';
 export default function Header() {
   const nav = useNavigation();
   return (
-    <View>
-      <View className='p-2 bg-gray-900 flex-row'>
-        <Pressable className='flex-row pl-2' onPress={() => { pressHome(); nav.navigate('Home') }}>
-          <Image source={require('../assets/img/bakal-body.jpg')}
-            className='h-12 w-12 rounded-full' />
-          <View className='justify-center pl-3'>
-            <Text className='text-lg text-white font-bold'>Unli Bakal</Text>
-          </View>
-        </Pressable>
-        <View className='justify-center pl-2'>
-          <Text className='justify-center text-white'>|</Text>
+    <View className='justify-center py-2 bg-gray-800 flex-row'>
+      <Pressable className='flex-row pl-2' onPress={() => { pressHome(); nav.navigate('Home') }}>
+        <View className='justify-center'>
+          <Text className='text-lg text-white font-bold'>Social</Text>
         </View>
-        <View className='justify-center pl-2'>
-          <Text className='text-white'>Go hard or go home!</Text>
+        <Image source={require('../assets/img/bakal-body.jpg')}
+          className='h-10 w-10 rounded-full mx-3' />
+        <View className='justify-center'>
+          <Text className='text-lg text-white font-bold'>Bakal</Text>
         </View>
-      </View>
-    </View >
+      </Pressable>
+    </View>
   )
 }
 
